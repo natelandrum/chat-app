a6ae1534698eee5a80aa43199cc6333b8239bb24
natelandrum <nate178dawg@gmail.com>
2025-04-08 15:10:03 -0600

    Only render return button when not on home page

--- app/components/chat/ReturnToChatList.tsx
+++ app/components/chat/ReturnToChatList.tsx
@@ -1,11 +1,25 @@
+"use client";
+
+import { usePathname } from 'next/navigation';
+
 import Link from 'next/link';
 import { Typography } from "@mui/material";
 import ArrowBackIosNew from "@mui/icons-material/ArrowBackIosNew";
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
 
 export default function ReturnToChatList() {
+  const pathname = usePathname();
+  const [isHomePage, setIsHomePage] = useState(false);
+
+  useEffect(() => {
+    setIsHomePage(pathname === "/");
+  }, [pathname]);
 
     return (
-      <div className="absolute hover:scale-110 left-0 top-24 p-4 w-fit">
+      <div className={clsx("absolute hover:scale-110 left-0 top-24 p-4 w-fit", {
+        hidden: isHomePage
+      })}>
         <Link href="/" className="flex items-center space-x-2">
           <ArrowBackIosNew />
           <Typography variant="h6">Back to Chat List</Typography>
