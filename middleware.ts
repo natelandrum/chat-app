ee312a1d23ba49cefc2d8f98fa844c6e91498c09
natelandrum <nate178dawg@gmail.com>
2025-04-08 14:11:30 -0600

    Logging middleware

--- middleware.ts
+++ middleware.ts
@@ -9,18 +9,22 @@ const DEFAULT_REDIRECT = '/';
 const ROOT = '/signin';
 
 export default auth((req) => {
-  const { nextUrl, url } = req;
+  const { nextUrl } = req;
 
   const isAuthenticated = !!req.auth;
   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
 
   try {
+    console.log('nextUrl.pathname:', nextUrl.pathname);
+    console.log('nextUrl.origin:', nextUrl.origin);
+    console.log('req.url:', req.url);
+
     if (isPublicRoute && isAuthenticated) {
-      return Response.redirect(new URL(DEFAULT_REDIRECT, req.url));
+      return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl.origin));
     }
 
     if (!isAuthenticated && !isPublicRoute) {
-      return Response.redirect(new URL(ROOT, req.url));
+      return Response.redirect(new URL(ROOT, nextUrl.origin));
     }
 
     return NextResponse.next();
