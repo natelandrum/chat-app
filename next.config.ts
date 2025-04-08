41a0f17a9917843d2426b729f26b06337a4a3154
natelandrum <nate178dawg@gmail.com>
2025-04-08 12:45:50 -0600

    App config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Allow images from specific domains
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
