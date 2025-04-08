4adee9be7eee5a4ef7be41d261888ada66538098
natelandrum <nate178dawg@gmail.com>
2025-04-08 13:50:09 -0600

    Middleware fix

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import { authOptions } from './app/auth.config';
+import { NextResponse } from 'next/server';
 
 const { auth } = NextAuth(authOptions);
 
@@ -7,20 +8,22 @@ const PUBLIC_ROUTES = ['/signin'];
 const DEFAULT_REDIRECT = '/';
 const ROOT = '/signin';
 
-
 export default auth((req) => {
- const { nextUrl } = req;
+  const { nextUrl } = req;
 
- const isAuthenticated = !!req.auth;
- const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+  const isAuthenticated = !!req.auth;
+  const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
 
- if (isPublicRoute && isAuthenticated)
-  return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
+  if (isPublicRoute && isAuthenticated) {
+    return Response.redirect(new URL(DEFAULT_REDIRECT, req.url));
+  }
 
- if (!isAuthenticated && !isPublicRoute)
-  return Response.redirect(new URL(ROOT, nextUrl));
+  if (!isAuthenticated && !isPublicRoute) {
+    return Response.redirect(new URL(ROOT, req.url));
+  }
+  return NextResponse.next();
 });
 
 export const config = {
- matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logo.png).*)'],
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|logo.png).*)'],
 };
